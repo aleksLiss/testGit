1ffe4e7987e448094ca92a9533b1ebfd7d7ec326
aleksLiss <darklisik.darklisik@yandex.ru>
2024-06-03 22:00:51 +0300

    2. Rename calc to getSum

--- src/main/java/org/example/MethodExample.java
+++ src/main/java/org/example/MethodExample.java
@@ -1,14 +1,15 @@
 package org.example;
 
 public class MethodExample {
-    public static double calc(int a, int b) {
+    public static double getSum(int a, int b) {
         return a + b;
     }
 
+
     public static void main(String[] args) {
-        MethodExample.calc(1, 2);
-        MethodExample.calc(3, 4);
-        MethodExample.calc(2, 2);
-        MethodExample.calc(-1, -2);
+        MethodExample.getSum(1, 2);
+        MethodExample.getSum(3, 4);
+        MethodExample.getSum(2, 2);
+        MethodExample.getSum(-1, -2);
     }
 }
