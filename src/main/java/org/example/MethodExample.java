b6c9e6bc7152735efc583a4050270a12d1d97edd
aleksLiss <darklisik.darklisik@yandex.ru>
2024-06-03 21:59:30 +0300

    1. Add class MethodExample and method calc

---
+++ src/main/java/org/example/MethodExample.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class MethodExample {
+    public static double calc(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        MethodExample.calc(1, 2);
+        MethodExample.calc(3, 4);
+        MethodExample.calc(2, 2);
+        MethodExample.calc(-1, -2);
+    }
+}
